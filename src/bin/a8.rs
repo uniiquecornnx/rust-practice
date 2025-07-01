910462baa641f9481915a32381556f26731e8803
uniiquecornnx <devisha18.003gupta@gmail.com>
2025-07-01 20:51:26 +0800

    struct + enum + match

---
+++ src/bin/a8.rs
@@ -0,0 +1,40 @@
+enum Flavor{
+    Vanilla,
+    Chocolate,
+    Mango
+}
+
+struct Drink{
+    flavor: Flavor,
+    fruit_oz: f64
+
+}
+
+fn print_drink(drink: Drink){
+    match drink.flavor{
+        Flavor::Chocolate => println!("chocolate"),
+        Flavor::Vanilla => println!("vanilla"),
+        Flavor::Mango => println!("mango"),
+    }
+    println!("{:?}" , drink.fruit_oz);
+}
+
+fn main(){
+    let drink = Drink{
+        flavor: Flavor::Chocolate,
+        fruit_oz: 12.0
+    };
+
+    
+    print_drink(drink);
+    let drink2 = Drink{
+        flavor: Flavor::Vanilla,
+        fruit_oz: 10.0
+    };
+    print_drink(drink2);
+    let drink3 = Drink{
+        flavor: Flavor::Mango,
+        fruit_oz: 15.0
+    };
+    print_drink(drink3);
+}
